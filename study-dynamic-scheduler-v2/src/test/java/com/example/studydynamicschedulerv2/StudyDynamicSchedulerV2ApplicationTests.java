f25bfb3464523984962f600eb9d24d7e1f3aca92
soonho <shyoon991@gmail.com>
2023-01-03 21:09:12 +0900

    Initial Commit v2

---
+++ study-dynamic-scheduler-v2/src/test/java/com/example/studydynamicschedulerv2/StudyDynamicSchedulerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studydynamicschedulerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDynamicSchedulerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
