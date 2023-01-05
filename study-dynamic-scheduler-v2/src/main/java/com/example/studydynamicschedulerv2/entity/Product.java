d89198ee1103b4a63299caadb66d6cfeaccad665
soonho <shyoon991@gmail.com>
2023-01-05 21:49:53 +0900

    add config jpa / batch

--- study-dynamic-scheduler-v2/src/main/java/com/example/studydynamicschedulerv2/entity/Product.java
+++ study-dynamic-scheduler-v2/src/main/java/com/example/studydynamicschedulerv2/entity/Product.java
@@ -2,14 +2,15 @@ package com.example.studydynamicschedulerv2.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Product extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     private String id;
     private String title;
 
