d0c88b8cca3aea35e3e8a493c267df224d5ab3c8
soonho <shyoon991@gmail.com>
2023-01-08 20:12:30 +0900

    add controller - ProductController

---
+++ study-dynamic-scheduler-v2/src/main/java/com/example/studydynamicschedulerv2/web/product/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.studydynamicschedulerv2.web.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("product")
+public class ProductController {
+
+    @RequestMapping(value = {"", "/"})
+    public String index() {
+        return "redirect:/product/list";
+    }
+
+    @RequestMapping("list")
+    public String list() {
+        return "product/list";
+    }
+
+    @RequestMapping("edit/{productId}")
+    public String edit(@PathVariable String productId) {
+        return "product/edit";
+    }
+
+    @RequestMapping("register")
+    public String register() {
+        return "product/register";
+    }
+
+}
