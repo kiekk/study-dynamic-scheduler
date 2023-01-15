fccd21305004232b774c811cc00205019a3d106d
soonho <shyoon991@gmail.com>
2023-01-15 17:26:49 +0900

    edit sample data & config jpa

--- study-dynamic-scheduler-v1/src/main/resources/data.sql
+++ study-dynamic-scheduler-v1/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO JOB_SCHEDULE (job_name, trigger_cron, active_yn)
+INSERT INTO JOB_SCHEDULE (id, job_name, trigger_cron, active_yn)
 VALUES
-    ('batchJob1', '0 * * * * ?', 'Y'),
-    ('batchJob2', '0 * * * * ?', 'Y');
+    ('1', 'batchJob1', '0 * * * * ?', 'Y'),
+    ('2', 'batchJob2', '0 * * * * ?', 'Y');
