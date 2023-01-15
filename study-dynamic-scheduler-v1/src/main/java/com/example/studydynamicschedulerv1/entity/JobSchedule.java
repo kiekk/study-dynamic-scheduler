4821f8d253f26b2de5bb1667d65debeb6f0c39ef
soonho <shyoon991@gmail.com>
2023-01-15 17:05:01 +0900

    add entity JobSchedule

---
+++ study-dynamic-scheduler-v1/src/main/java/com/example/studydynamicschedulerv1/entity/JobSchedule.java
@@ -0,0 +1,50 @@
+package com.example.studydynamicschedulerv1.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class JobSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String jobName;
+    private String triggerCron;
+    private String activeYn;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getTriggerCron() {
+        return triggerCron;
+    }
+
+    public void setTriggerCron(String triggerCron) {
+        this.triggerCron = triggerCron;
+    }
+
+    public String getActiveYn() {
+        return activeYn;
+    }
+
+    public void setActiveYn(String activeYn) {
+        this.activeYn = activeYn;
+    }
+}
